f3afc13d23acf9b83731e1c8e0be1a819761273c
Matthew Fricke <fricke9523@msn.com>
2017-10-04 05:48:45 -0600

    Checkpoint 14 Bloc Jams Sliders Complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
 		$(this).attr("playState", player.playState);
 	});
 
+	$("button#previous").click( function() {
+		if(player.playState !== "playing") {return;}
+
+		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+		const previousSongIndex = currentSongIndex - 1;
+		if(previousSongIndex <= album.songs.indexOf(0)) {return;}
+
+		const previousSong = album.songs[previousSongIndex];
+		player.playPause(previousSong);
+	});
+
 	$("button#next").click( function() {
 		if(player.playState !== "playing") {return;}
 
@@ -15,15 +26,16 @@ $(document).ready(function(){
 		player.playPause(nextSong);
 	});
 
-	$("button#previous").click( function() {
-		if(player.playState !== "playing") {return;}
-
-		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-		const previousSongIndex = currentSongIndex - 1;
-		if(previousSongIndex <= album.songs.indexOf(0)) {return;}
-
-		const previousSong = album.songs[previousSongIndex];
-		player.playPause(previousSong);
+	$("#time-control input").on("input", function (event) {
+		player.skipTo(event.target.value);
 	});
+
+	setInterval( () => {
+		const currentTime = player.getTime();
+		const duration = player.getDuration();
+		const percent = (currentTime / duration) * 100;
+		$("#time-control .current-time").text(currentTime);
+		$("#time-control input").val(percent);
+	}, 1000);
 
 });
