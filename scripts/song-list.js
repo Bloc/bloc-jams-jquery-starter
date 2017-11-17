186d504756f4756d98a304546a8f36c67455707e
DeAnne <indeannajones@yahoo.com>
2017-11-17 03:57:09 -0500

    Assignment 13 completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -3,12 +3,12 @@
      song.element = $(`
        <tr>
          <td>
-            <button>
-              <span class="song-number">${index + 1}</span>
-              <span class="ion-play"></span>
-              <span class="ion-pause"></span>
-            </button>
-          </td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </button>
+         </td>
          <td>${song.title}</td>
          <td>${song.duration}</td>
         </tr>
