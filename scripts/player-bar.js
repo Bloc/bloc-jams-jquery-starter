88c86a996a971be48ebf0ad8249c88a474f71391
Katherine Goodman <kgoodman131@gmail.com>
2017-12-04 11:08:35 -0800

    Assignment 13 work: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,4 +14,15 @@
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex >= album.songs.length) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
 }
