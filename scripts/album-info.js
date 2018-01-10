37a00c66b02c82bcbb344bc2ecdeccad3ed9b4b6
Brent Phillips <poposhub@gmail.com>
2018-01-10 04:32:56 -0500

    Added artist and release info

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
 }
