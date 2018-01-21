b5f4a3179d093dcfb057dfba5ef67289357a96db
webdevbrent <webdeveloperbrent@gmail.com>
2018-01-21 08:30:17 -0500

    Cleaned up code

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -26,4 +26,17 @@
     player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text( currentTime );
+     $('#time-control input').val(percent);
+   }, 1000);
+
 }
