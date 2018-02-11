7d26b3394ad69de055d0ea054bf555cc927b6638
Irina Sverzhanovskaya <32099518+irina6793@users.noreply.github.com>
2018-02-11 10:49:20 -0800

    Update album-info.js

---
+++ scripts/album-info.js
@@ -0,0 +1,12 @@
+<!-- Album Info  -->
+<section id="album-info">
+  <!-- Album Image  -->
+  <img id="album-cover-art">
+  <!-- Album Details -->
+  <div class="album-details">
+    <h1 id="album-title"></h1>
+    <h2 class="artist"></h2>
+    <div id="release-info"></div>
+  </div>
+</section>
+ 
