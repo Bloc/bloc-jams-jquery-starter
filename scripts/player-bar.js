f408e872dc7448ef02f097727d8c475ea09165a5
Irina Sverzhanovskaya <32099518+irina6793@users.noreply.github.com>
2018-02-17 13:41:35 -0800

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,3 +1,4 @@
+
 {
    $('button#play-pause').on('click', function() {
      player.playPause();
@@ -11,5 +12,43 @@
    if (nextSongIndex >= album.songs.length) { return; }
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
+      $('#prev').click(function() {
+         audio.pause();
+         var prev = $('#playlist tr.active'). prev();
+         if(prev.length == 0) {
+            prev = $('playlist tr:last-child');
+         }
+         initAudio(prev);
+         audio.play();
+         showDuration();
+         $('#play).hide();
+           $('#pause').show;
+      });
+      $('button#previous').on('click', function() {
+         var clickHandler = function() {
+         if (currentlyPlayingSongNumber !== null){
+            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+            currentlyPlayingCell.html(currentlyPlayingSongNumber);
+        }
+        if (currentlyPlayingSongNumber !== songNumber){
+            $(this).html(pauseButtonTemplate);
+            setSong(songNumber);
+            currentSoundFile.play();
+            updateSeekBarWhileSongPlays();
+            updatePlayerBarSong();
+
+        } else if (currentlyPlayingSongNumber === songNumber){
+            if (currentSoundFile.isPaused()){
+                $(this).html(pauseButtonTemplate);
+                currentSoundFile.play();
+                updateSeekBarWhileSongPlays();
+            }
+            else{
+                currentSoundFile.pause();
+            }
+        }
+    };
+      
+      
    });
  }
