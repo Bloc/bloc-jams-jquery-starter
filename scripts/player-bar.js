3ab5290823f8c398789be7ce5908406196f16eb1
Irina Sverzhanovskaya <32099518+irina6793@users.noreply.github.com>
2018-02-18 10:14:57 -0800

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -5,42 +5,41 @@
   });
    $('button#next').on('click', function() {
    if (player.playState !== 'playing') { return; }
-   
    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
    const nextSongIndex = currentSongIndex + 1;
    if (nextSongIndex >= album.songs.length) { return; }
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
-   });
+  });
    $('#time-control input').on('input', function (event) {
       player.skipTo(event.target.value);
-   });
-   setInterval( () => {
-      if (player.playState !== 'playing') { return; }
-      const currentTime = player.getTime();
+  });
+     setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
-       $('#time-control input').val(percent);
-       $('#time-control .current-time').text( currentTime );
+     $('#time-control input').val(percent);
+     $('#time-control .current-time').text( currentTime );
    }, 1000);
       $('#prev').click(function() {
-         audio.pause();
-         var prev = $('#playlist tr.active'). prev();
-         if(prev.length == 0) {
-            prev = $('playlist tr:last-child');
-         }
-         initAudio(prev);
-         audio.play();
-         showDuration();
-         $('#play).hide();
-           $('#pause').show;
-      });
+      audio.pause();
+      var prev = $('#playlist tr.active'). prev();
+      if(prev.length == 0) {
+     prev = $('playlist tr:last-child');
+  }
+     initAudio(prev);
+     audio.play();
+     showDuration();
+     $('#play).hide();
+     $('#pause').show;
+  });
       $('button#previous').on('click', function() {
-         var clickHandler = function() {
-         if (currentlyPlayingSongNumber !== null){
-            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
-            currentlyPlayingCell.html(currentlyPlayingSongNumber);
-        }
+      var clickHandler = function() {
+      if (currentlyPlayingSongNumber !== null){
+      var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+       currentlyPlayingCell.html(currentlyPlayingSongNumber);
+   }
         if (currentlyPlayingSongNumber !== songNumber){
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
