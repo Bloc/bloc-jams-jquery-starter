c85c841e037f3030a2d29af7247d2fe585b3b1a4
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-03-08 16:10:09 -0600

    back ticks update

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,6 +1,6 @@
 {
   album.songs.forEach( (song, index) => {
-   song.element = $('
+   song.element = $(`
      <tr>
       <td>
         <button>
@@ -12,8 +12,8 @@
       <td>${song.title}</td>
       <td>${song.duration}</td>
      </tr>
-   ');
-                    
+   `);
+
    song.element.on('click', event => {
      player.playPause(song);
    });
