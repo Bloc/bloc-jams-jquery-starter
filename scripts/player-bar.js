4132a7393253689132f6b98698ae8e0f0fdcd459
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-03-20 17:29:46 -0500

    playerbar sliders

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -23,4 +23,17 @@
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playerState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+        $('#time-control .current-time').text( currentTime );
+        $('#time-control input').val(percent);
+  }, 1000);
 }
