d8df8b7dabefbd439d3de8c11b1cf2859d27d114
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-03-20 15:50:21 -0500

    playerbar button update

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,6 +4,7 @@
     $(this).attr('playState', player.playState);
   });
 
+
   $('button#next').on('click', function(){
     if (player.playerState !== 'playing') { return; }
 
@@ -14,7 +15,6 @@
     player.playPause(nextSong);
   });
 
-{
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
@@ -24,13 +24,4 @@
     player.playPause(previousSong);
   });
 
-=======
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-    
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
-  });
 }
