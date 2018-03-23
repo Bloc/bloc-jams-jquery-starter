24eab871016cae2f9395bce35e293630b56ba19d
dotmao <dorothy.hongqian@gmail.com>
2018-03-23 09:09:44 -0400

    updated files 3/23

---
+++ scripts/album-info.js
@@ -0,0 +1,8 @@
+{
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
+  $('song-number-column').inArray(album.songs);
+
+}
