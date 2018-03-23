6ed003727c613601c5263d1e0ff6d0b6374dabc4
dotmao <dorothy.hongqian@gmail.com>
2018-03-23 09:14:44 -0400

    deleted a line

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,6 +3,5 @@
   $('img#album-cover-art').attr('src', album.albumArtUrl);
   $('.artist').text(album.artist);
   $('#release-info').text(album.releaseInfo);
-  $('song-number-column').inArray(album.songs);
 
 }
