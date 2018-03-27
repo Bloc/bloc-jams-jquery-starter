09ab20f40e5be03b187f0c19df080663d85d1fc3
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-03-27 17:57:03 -0500

    added ; to return

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,10 +16,10 @@
   });
 
   $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return }
+    if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex === -1) { return }
+    if (previousSongIndex === -1) { return; }
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
