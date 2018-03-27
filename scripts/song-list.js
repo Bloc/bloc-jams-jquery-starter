5241ed06ac60b92ce015703dad5bd326ec288703
dotmao <dorothy.hongqian@gmail.com>
2018-03-27 09:36:42 -0400

    modified js 03/27

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,13 +1,24 @@
 {
   //using forEach method to repeat through album.songs
-  album.songs.forEach( (song, index) => {
+
+   album.songs.forEach( (song, index) => {
     song.element = $(`
-      <tr>
-         <td>${index + 1}</td> 
+      <tr>  
+        <td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </button>
+         </td>
          <td>${song.title}</td>
          <td>${song.duration}</td>
         </tr>
       `);
+
+      song.element.on('click', event => {
+        player.playPause(song);
+      })
     //append elements to HTML
     $('#song-list').append(song.element);
   });
