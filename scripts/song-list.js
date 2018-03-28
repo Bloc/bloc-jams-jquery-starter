002b0fd0bd21fbcd0a8c8b3c74181dabdecad8c4
dotmao <dorothy.hongqian@gmail.com>
2018-03-28 17:03:45 -0400

    update all

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -2,8 +2,9 @@
   //using forEach method to repeat through album.songs
 
    album.songs.forEach( (song, index) => {
+// song.element > .element just means we are going inside of `song` to retrieve its properties.
     song.element = $(`
-      <tr>  
+      <tr>
         <td>
            <button>
              <span class="song-number">${index + 1}</span>
@@ -15,9 +16,13 @@
          <td>${song.duration}</td>
         </tr>
       `);
+      console.log(song);
 
       song.element.on('click', event => {
         player.playPause(song);
+
+    //syncing the play button on both play bar and icon in front of the song.
+        $('button#play-pause').attr('playState', player.playState);
       })
     //append elements to HTML
     $('#song-list').append(song.element);
