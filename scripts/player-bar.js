3a705abeb2ee981c8e3c4c987a0371038bcfddd8
Drew Disterhoft <drew.disterhoft@gmail.com>
2018-04-09 20:51:24 -0500

    assignment-next previous button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,7 +16,7 @@
   });
 
   $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+    if (player.playerState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
     if (previousSongIndex < 0) { return; }
