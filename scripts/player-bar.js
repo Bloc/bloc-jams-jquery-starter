7d3157416792aba5197cc22b508d0a2e014283ba
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-07-28 20:33:32 -0500

    Completed assignment

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,7 +16,7 @@ $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex = ) { return; }
+    if (previousSongIndex < 0) { return; }
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
 });
