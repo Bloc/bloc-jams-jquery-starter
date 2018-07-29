ee2e91a93d7ee299d0139bfb115f2bfbd5ca1123
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-07-28 20:21:26 -0500

    previous player bar error

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,12 +4,20 @@
     $(this).attr('playState', player.playState);
   });
 $('button#next').on('click', function() {
-  console.log('hello');
     if (player.playState !== 'playing') {return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+});
+$('button#previous').on('click', function() {
+   console.log('hello');
+    if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex = ) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
 });
 }
