d76abe56d827694096195eaae0ce116779ca0b71
Steph <stephjogreen@gmail.com>
2018-08-07 09:24:31 -0500

    playerbar assignment completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,4 +14,16 @@
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click', function (){
+  if (player.playState !== 'playing') {return;}
+
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const previousSongIndex = currentSongIndex - 1;
+  if (previousSongIndex <= -1) {return;}
+
+  const previousSong = album.songs[previousSongIndex];
+  player.playPause(previousSong);
+});
+
 }
