f5173c13087857431a04cc8499198101d574a0a5
pakajiobi <35235726+pakajiobi@users.noreply.github.com>
2018-08-13 13:22:04 -0700

    assignment 9

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -15,7 +15,6 @@
     `);
      song.element.on('click', event => {
       player.playPause(song);
-      $('button#play-pause').attr('playState', player.playState);
     });
      $('#song-list').append(song.element);
   });
