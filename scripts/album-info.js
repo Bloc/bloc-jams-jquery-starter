40a90487b07a5715a9b997f65acc73f41cc55efb
Gia Cheng <jcheng.4869@gmail.com>
2018-09-28 09:24:08 +0800

    assignment 8 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,7 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
+
 }
