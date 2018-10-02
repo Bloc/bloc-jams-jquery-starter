e3fad259d39d8fd8b68f1902a85c092da6420685
Gia Cheng <jcheng.4869@gmail.com>
2018-10-02 11:22:16 +0800

    checkpoint-songlist completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -7,7 +7,7 @@
               <span class="song-number">${index + 1}</span>
               <span class="ion-play"></span>
               <span class="ion-pause"></span>
-            /button>
+            </button>
         </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
