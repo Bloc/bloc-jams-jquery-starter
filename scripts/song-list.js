58cb570a6187217e3e41377e7b60285beceaa55b
Gia Cheng <jcheng.4869@gmail.com>
2018-10-03 22:12:20 +0800

    chekcpoint playbarbuttons completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -17,6 +17,8 @@
 
      song.element.on('click', event => {
        player.playPause(song);
+       $('button#play-pause').attr('playState', player.playState);
+
      });
 
           $('#song-list').append(song.element);
