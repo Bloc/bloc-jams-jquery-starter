9865fceb10fe5e281acf76fe13cd6441a56e0ca9
Fred <fredk@alumni.usc.edu>
2018-10-24 15:53:01 -0700

    assignment-8

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,6 @@
 {//mapping album object to album info html via jQuery
 $('#album-title').text(album.title);
 $('img#album-cover-art').attr('src', album.albumArtUrl);
-
+$('.artist').text(album.artist);
+$('#release-info').text(album.releaseInfo);
 }
