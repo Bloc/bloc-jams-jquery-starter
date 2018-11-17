570b02baf16c436b09dc1dc97fb54e54d488e40e
vnice6 <vincerodriguez13@gmail.com>
2018-11-17 08:46:50 -0800

    previous button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,8 +10,20 @@
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click',function(){
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (prevSongIndex >= album.songs.length) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
+  });
+
 }
