431851d9fcd2ee7d62604ffafc5498af5570e47d
Rico Pananganan <fpananganan@gmail.com>
2018-12-11 11:33:56 -0800

    Assignment 11 Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -24,7 +24,11 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime ); 
+    $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
+
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  }); 
 }
