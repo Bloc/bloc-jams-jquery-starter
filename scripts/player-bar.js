630cd97232aeb958712f68efdf3ffbc86262f4e6
Key Richey <keyona.richey@gmail.com>
2018-12-18 11:33:41 -0800

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,7 +14,7 @@
   $('button#previous').on('click', function(){
     if (player.playState !== 'playing') { return; }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const previousSongIndex = currentSongIndex + -1;
+    const previousSongIndex = currentSongIndex - 1;
     if (previousSongIndex < 0 ) { return; }
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
