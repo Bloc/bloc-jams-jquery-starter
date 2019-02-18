471ff008f94460979472aff0aef34273a2e2130a
Eunice Son <eunicejson@gmail.com>
2019-02-17 19:25:08 -0600

    revised jams2

---
+++ scripts/album-info.js
@@ -0,0 +1,7 @@
+{
+  $('#album-title').text(album.title);
+     $('img#album-cover-art').attr('src', album.albumArtUrl);
+     $('#release-info').text(album.releaseInfo);
+     $('.artist').text(album.artist);
+     
+}
