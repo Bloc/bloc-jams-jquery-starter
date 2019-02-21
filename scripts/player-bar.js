8fefd0b31405a6896c8e1ac9ae3b75a410f0e43b
Eunice Son <eunicejson@gmail.com>
2019-02-20 21:54:42 -0600

    hw06

---
+++ scripts/player-bar.js
@@ -0,0 +1,15 @@
+{
+  $('button#play-pause').on('click', function() {
+       player.playPause();
+        $(this).attr('playState', player.playState);
+     });
+
+     $('button#next').on('click', function() {
+       if (player.playState !== 'playing') { return; }
+
+       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+       const nextSongIndex = currentSongIndex + 1;
+       const nextSong = album.songs[nextSongIndex];
+       player.playPause(nextSong);
+     });
+}
