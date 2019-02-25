5d9cbf82cc598bac3a7c0d9c9510ea1d8cf6ad04
Eunice Son <eunicejson@gmail.com>
2019-02-25 12:33:34 -0600

    revised hw06

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,12 +4,26 @@
         $(this).attr('playState', player.playState);
      });
 
-     $('button#next').on('click', function() {
-       if (player.playState !== 'playing') { return; }
+   $('button#next').on('click', function() {
+     if (player.playState !== 'playing') { return; }
 
-       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-       const nextSongIndex = currentSongIndex + 1;
-       const nextSong = album.songs[nextSongIndex];
-       player.playPause(nextSong);
-     });
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const nextSongIndex = currentSongIndex + 1;
+     if (nextSongIndex >= album.songs.length) { return; }
+
+     const nextSong = album.songs[nextSongIndex];
+     player.playPause(nextSong);
+   });
+
+   $('button#previous').on('click', function() {
+     if (player.playState !== 'playing') { return; }
+
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const previousSongIndex = currentSongIndex - 1;
+     if (previousSongIndex < 0) { return; }
+
+     const previousSong = album.songs[previousSongIndex];
+     player.playPause(previousSong);
+
+   });
 }
