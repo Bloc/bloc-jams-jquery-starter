cfd74ba16c2a5e85e849302d1ad9053db8fe4bdd
Eunice Son <eunicejson@gmail.com>
2019-03-01 13:09:33 -0600

    new helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,7 +2,11 @@ class Helper{
   playPauseAndUpdate(song){
     player.playPause(song);
 
+  if (song) {
     $('#time-control .total-time').text(song.duration);
+  } else {
+      $('#time-control .total-time').text(player.currentlyPlaying.duration);
+    }
   }
 }
 
