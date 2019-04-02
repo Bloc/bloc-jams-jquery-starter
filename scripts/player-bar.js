da0ab4b204c17c8a6e27b08a81ce08b6c4cbd588
Jessica Zhong <jesssicazhong@gmail.com>
2019-04-02 13:54:52 -0700

    assignment-10-frontend

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -9,9 +9,20 @@
 
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
         const nextSongIndex = currentSongIndex +1;
-        if (nextSongIndex >= albums.songs.length) { return; }
+        if (nextSongIndex >= album.songs.length) { return; }
         
         const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
+    });
+
+    $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const previousSongIndex = currentSongIndex -1;
+        if (previousSongIndex >= album.songs.length) { return; }
+
+        const previousSong = album.songs[previousSongIndex];
+        player.playPause(previousSong);
     });
 }
