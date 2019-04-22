429b91d58e79b8b9f6fe7298c3d137d912aae380
uo <schoolgirluche@gmail.com>
2019-04-22 14:34:19 -0700

    added to album-info.js

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,8 +1,7 @@
 {
     $('#album-title').text(album.title);
-}
-
-{
-    $('#album-title').text(album.title);
+    $('.artist').text(album.artist);
     $('img#album-cover-art').attr('src', album.albumArtUrl);
-}
+    $('#release-info').text(album.releaseInfo);
+
+  }
