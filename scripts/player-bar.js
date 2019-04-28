4d998f4b6f332477ca3fbe2dc8ea406b1a25f7cf
Prabha Ganapathi <prabha.ux@gmail.com>
2019-04-27 22:12:46 -0700

    All Files Updated

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,7 +16,6 @@ $('button#previous').on('click', function() {
   if (player.playState !== 'playing') { return; }
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
   const previousSongIndex = currentSongIndex - 1;
-  if (previousSongIndex <= album.songs.length) { return; }
   const previousSong = album.songs[previousSongIndex];
   player.playPause(previousSong);
 });
