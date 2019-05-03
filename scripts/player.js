088f83c31948cde4bb4cf604d700f49d31741556
Inna <innabeltcova@gmail.com>
2019-05-03 09:41:11 -0400

    mapped the artist and release info

--- scripts/player.js
+++ scripts/player.js
@@ -1,3 +1,4 @@
+
 class Player {
   constructor () {
     this.currentlyPlaying = album.songs[0];
@@ -13,14 +14,14 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
@@ -37,12 +38,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +51,3 @@ class Player {
 }
 
 const player = new Player();
-
