a76e23b7ace085a02c88316ee81f74f3aa1d93b8
reemicz <reemicz@gmail.com>
2019-05-07 17:15:10 -0400

    create previous button (copy button#play-pause) reverse function -1

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,5 +3,4 @@
      $('img#album-cover-art').attr('src', album.albumArtUrl);
      $('.artist').text(album.artist);
      $('#release-info').text(album.releaseInfo);
-
 }
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,10 +10,25 @@
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const nextSongIndex = currentSongIndex + 1;
      if (nextSongIndex >= album.songs.length) { return; }
- 
+
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
    });
- }
+
+//}
+// remember to remove }); from above lines - for code to work
+// adding previous button for track 05/07/2019 (May07) ^ copy above and reverse
+// to make previous button
+
+   $('button#previous').on('click', function () {
+      if (player.playState!== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex < 0) { return; }
+
+      const previousSong = album.songs[previousSongIndex];
+
+      player.playPause(previousSong);
    });
-}
+ }
