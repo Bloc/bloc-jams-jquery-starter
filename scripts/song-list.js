eafefc031e04d39748bdd09f93258d9c5095eb01
reemicz <reemicz@gmail.com>
2019-05-07 15:21:58 -0400

    Changes to scripts/song-list.js and index.html

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -21,3 +21,28 @@
     $('#song-list').append(song.element);
   });
 }
+// code above this line done 05/06/2019 - code below is 05/07/2019
+//
+// {
+//   album.songs.forEach( (song, index) => {
+//     song.element = $(`
+//       <tr>
+//       <td>
+//            <button>
+//              <span class="song-number">${index + 1}</span>
+//              <span class="ion-play"></span>
+//              <span class="ion-pause"></span>
+//            </button>
+//          </td>
+//       <td>${song.title}</td>
+//       <td>${song.duration}</td>
+//        </tr>
+//     `);
+//
+//     song.element.on('click', event => {
+//       player.playPause(song);
+//     });
+//
+//        $('#song-list').append(song.element);
+//   });
+// }
