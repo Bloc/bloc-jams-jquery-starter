4cb71b13592875066728be3053346d359c03c770
valentinavega <valentinavega94@gmail.com>
2019-05-19 17:04:12 -0400

    Update

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,2 +1,13 @@
 {
+    $('#album-title').text(album.title);
+    $('img#album-cover-art').attr('src', album.albumArtUrl);
+    $('.artist').text(album.artist);
+    $('#release-info').text(album.releaseInfo);
 }
+
+//{
+//    $('#album-title').text(album.title);
+//    $('img#album-cover-art').attr('src', album.albumArtUrl);
+//    $('#artist').text(album.artist);
+//    $('#release-info').text(album.releaseInfo);
+//}
