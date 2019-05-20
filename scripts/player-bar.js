bc6f493e7718a5c84d9f5d0c4b4c4190b9ed9669
Megan Harrington <mewalsh629@gmail.com>
2019-05-20 18:19:39 -0400

    checkpoint complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -12,8 +12,23 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+    });
 
-  });
+    $('#time-control input').on('input', function (event) {
+      player.skipTo(event.target.value);
+      });
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+      }, 1000);
+
+
+
 
     $('button#previous').on('click', function() {
       if (player.playState !== 'playing') { return; }
@@ -26,5 +41,7 @@
       player.playPause(previousSong);
 
 });
+
+
 
 }
