9103c859589881a1852e58bd16195ebc5e0cb985
Megan Harrington <mewalsh629@gmail.com>
2019-05-20 10:24:14 -0400

    checkpoint-13-16 complete

--- scripts/player.js
+++ scripts/player.js
@@ -13,18 +13,18 @@ class Player {
   getTime() {
     return this.soundObject.getTime();
   }
-  
+
   playPause (song = this.currentlyPlaying) {
     if (this.currentlyPlaying !== song) {
       // Stop the currently playing sound file (even if nothing is playing)
       this.soundObject.stop();
       // Clear classes on the song that's currently playing
       this.currentlyPlaying.element.removeClass('playing paused');
-      
+
       // Update our currentlyPlaying and playState properties
       this.currentlyPlaying = song;
       this.playState = 'stopped';
-      this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
+    *  this.soundObject = new buzz.sound(this.currentlyPlaying.soundFileUrl);
     }
     if (this.playState === 'paused' || this.playState === 'stopped') {
       this.soundObject.setVolume( this.volume );
@@ -37,12 +37,12 @@ class Player {
       this.currentlyPlaying.element.removeClass('playing').addClass('paused');
     }
   }
-  
+
   skipTo (percent) {
     if (this.playState !== 'playing') { return }
     this.soundObject.setTime( (percent / 100) * this.soundObject.getDuration() );
   }
-  
+
   setVolume (percent) {
     this.volume = percent;
     this.soundObject.setVolume(percent);
@@ -50,4 +50,3 @@ class Player {
 }
 
 const player = new Player();
-
---
+++ scripts/song-list.js
@@ -0,0 +1,19 @@
+{
+  album.songs.forEach( (song, index) => {
+    song.element = $(`
+      <tr>
+        <td>
+          <button>
+            <span class="song-number">${index + 1}</span>
+            <span class="ion-play"></span>
+            <span class="ion-pause"></span>
+          </button>
+        </td>
+        <td>${song.title}</td>
+        <td>${song.duration}</td>
+       </tr>
+     `);
+
+     $('#song-list').append(song.element);
+  });
+}
