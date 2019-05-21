9b948fcd56b7990e3a06880652526c994d95a917
reemicz <reemicz@gmail.com>
2019-05-21 11:11:32 -0400

    changes to jquery information

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,5 +1,21 @@
-class Helper {
+// class Helper {
+//   // class=playPauseAndUpdate parameter=song
+//     playPauseAndUpdate(song) {
+//         const duration = player.getDuration();
+//         $('#time-control .total-time').text(duration);
+//         player.playPause(song);
+//     }
+// <<<<<<< HEAD
+//     $('#time-control .total-time').text(totalTime);
+//   };
+// =======
+// >>>>>>> check11
+// }
+// const helper = new Helper();
 
+
+class Helper {
+  // class=playPauseAndUpdate parameter=song
     playPauseAndUpdate(song) {
         const duration = player.getDuration();
         $('#time-control .total-time').text(duration);
