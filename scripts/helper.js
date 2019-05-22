71bb0d8b9046d5fd0e49176d2066d158d9ea48bd
Megan Harrington <47283891+meganharrington@users.noreply.github.com>
2019-05-22 11:33:31 -0400

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,7 +2,7 @@ class Helper {
   playPauseAndUpdate(song) {
     player.playPause (song);
     const totalTime = player.getDuration(song);
-    $(".total-time").text(totalTime);
+    $('#total-time.totaltime').text(totalTime);
   }
 
 }
