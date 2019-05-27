0692eda2f25d7399b220436336f7463a9d350cc4
Megan Harrington <mewalsh629@gmail.com>
2019-05-27 17:32:30 -0400

    final updates

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -23,7 +23,7 @@
 
       const previousSong = album.songs[previousSongIndex];
       helper.playPauseAndUpdate(previousSong);
-});
+    });
 
     $('#time-control input').on('input', function (event) {
       player.skipTo(event.target.value);
