8224150d35b4c815e3a620ba803bdd690abe5529
shuchisd@gmail.com <shuchisd@gmail.com>
2019-05-27 22:08:15 -0400

    This commit implements below functionalities:
    The Previous Track button only responds when a song is currently playing.
    The Previous Track button plays the previous song on the album.
    The Previous Track button doesn't respond if there is no previous song.

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -11,16 +11,26 @@
    // Add event handler for previous button to play previous song in the list unless the song being played
    // is the first song
     $('button#previous').on('click', function() {
-      if (player.playState !== 'playing') { return; }
-      // Set value of attribute 'playState' of the button to whatever is the value of
-      // player.playState
-      $(this).attr('playState', player.playState);
-    });
 
+      // If no song is being played, do nothing
+      if (player.playState !== 'playing') { return; }
+
+      // Get the index of the current song being played
+       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+       const previousSongIndex = currentSongIndex - 1;
+
+       // Check if the song being played is last by comparing it with the length of album.songs array
+       if (previousSongIndex < 0) { return;}
+
+       const previousSong = album.songs[previousSongIndex];
+       player.playPause(previousSong);
+    });
 
     // Add event handler for next button to play next song in the list unless the song being played
     // is the last song
      $('button#next').on('click', function() {
+
+       // If no song is being played, do nothing
        if (player.playState !== 'playing') { return; }
 
       // Get the index of the current song being played
