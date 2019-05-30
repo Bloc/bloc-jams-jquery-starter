827b0aeb39a12dd03c97cb7c6c769914c48e520a
reemicz <reemicz@gmail.com>
2019-05-30 13:43:40 -0400

    final changes to helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -13,11 +13,10 @@
 // }
 // const helper = new Helper();
 
-
 class Helper {
     playPauseAndUpdate(song) {
         player.playPause(song);
-        const duration = player.getDuration();
+        const duration = player.currentlyPlaying.duration;
         $('#time-control .total-time').text(duration);
 
     }
