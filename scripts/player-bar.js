9272f95899212e9197702c9a5190be2e97275a2b
Karin Carlson <38005879+karin-carlson@users.noreply.github.com>
2019-05-30 11:32:37 -0400

    moved player-bar to assets folder

---
+++ scripts/player-bar.js
@@ -0,0 +1,47 @@
+ {
+   $('button#play-pause').on('click', function() {
+     player.playPause();
+     $(this).attr('playState', player.playState);
+   }); 
+     
+   $('button#next').on('click', function() {
+        if (player.playState !== 'playing') { return; } 
+    
+       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+       const nextSongIndex = currentSongIndex + 1;
+            if (nextSongIndex >= album.songs.length) { return; }
+ 
+       const nextSong = album.songs[nextSongIndex];
+     player.playPause(nextSong);
+   });    
+     
+   $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
+   });
+     
+     setInterval( () => {
+     if (player.playState !== 'playing') { return; }
+     const currentTime = player.getTime();
+     const duration = player.getDuration();
+     const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text( currentTime );
+     $('#time-control input').val(percent);
+   }, 1000);
+     
+     $('#volume-control input').on('input', function (event) {
+        player.setVolume(event.target.value);
+  });
+     
+     
+  $('button#previous').on('click', function () {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex >= album.songs.length) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
+
+ }
