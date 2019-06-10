6382d2c4cb098920becca275e63847f7d40f4c9c
Leanna Marsh <39321894+leanna-marsh@users.noreply.github.com>
2019-06-10 14:52:53 -0700

    push1

---
+++ scripts/song-list.js
@@ -0,0 +1,13 @@
+{
+  album.songs.forEach( (song, index) => {
+    song.element = $(`
+  <tr>
+  <td>${index}</td>
+  <td>${song.title}</td>
+  <td>${song.duration}</td>
+   </tr>
+`);
+
+    $('#song-list').append(song.element);
+});
+}
