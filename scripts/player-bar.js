03a31192c5bafefe20f8142c208e116c3f60668e
Leanna Marsh <39321894+leanna-marsh@users.noreply.github.com>
2019-06-10 15:20:18 -0700

    TimeSlider

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -15,6 +15,20 @@
      player.playPause(nextSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+
+  }, 1000);
+
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
 
