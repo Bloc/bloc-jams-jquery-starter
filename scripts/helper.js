9974c7378cc212d2a560e066459c78cd491907c7
Leanna Marsh <39321894+leanna-marsh@users.noreply.github.com>
2019-06-10 16:05:16 -0700

    Push4

---
+++ scripts/helper.js
@@ -0,0 +1,8 @@
+class Helper {
+
+    playPauseAndUpdate(song) {
+      player.playPause(song);
+      const totalTime = player.currentlyPlaying.duration;
+       $('#time-control .total-time').text(totalTime);
+};
+}
