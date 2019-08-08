9337c8a2fcae4c4e08460244513cae81c9e4b931
Katie Snead <kat.j.snead@gmail.com>
2019-08-08 12:01:00 -0700

    Checkpoint 10 Player Bar exercise complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -2,14 +2,12 @@
   $('button#play-pause').on('click', function () {
     player.playPause();
     $(this).attr('playState', player.playState);
-
   });
 
   $('button#next').on('click', function () {
     if (player.playState !== 'playing') {
       return;
     }
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) {
@@ -18,5 +16,19 @@
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('button#previous').on('click', function () {
+    if (player.playState !== 'playing') {
+      return;
+    }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0) {
+      return;
+    }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
   });
 }
