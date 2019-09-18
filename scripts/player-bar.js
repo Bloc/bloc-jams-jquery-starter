0cbb7f50a668930149c4e2183492c6a37c5ed264
Joslyn Tran <44991415+JoslynT@users.noreply.github.com>
2019-09-18 00:22:43 -0700

    Update player-bar.js

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -25,6 +25,25 @@ if (nextSongIndex >= album.songs.length) { return; }
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
 
+ $('#time-control input').on('input', function (event) {
+   player.skipTo(event.target.value);
+ });
+
+ $('#volume-control input').on('input', function (event) {
+   player.setVolume(event.target.value);
+ });
+
+ setInterval( () => {
+   if (player.playState !== 'playing') { return; }
+
+      const currentTime = player.getTime();
+   const duration = player.getDuration();
+   const percent = (currentTime / duration) * 100;
+   $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+
+
   });
 
 }
