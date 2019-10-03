1f54ea367c969af35c3db11b7b15174c0a3c415e
Allan klar <allanklar@hotmail.com>
2019-10-03 09:26:21 -0500

    checkpoint-blocjams-playerbar: completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,6 +16,7 @@
 
      song.element.on('click', event => {
        player.playPause(song);
+       $('button#play-pause').attr('playState', player.playState);
      });
 
      $('#song-list').append(song.element);
