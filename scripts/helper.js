fad4c29ee6b63a28bc37768d1c2b58e899dce4f8
Allan klar <allanklar@hotmail.com>
2019-10-07 12:50:02 -0500

    add new files

---
+++ scripts/helper.js
@@ -0,0 +1,18 @@
+  
+class Helper{
+
+  playPauseAndUpdate (song) {
+
+    player.playPause(song);
+
+    const totalTime = player.currentlyPlaying.duration;
+
+    $('#time-control .total-time').text( totalTime );
+
+  }
+
+}
+
+
+
+const helper = new Helper();
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-       player.playPause();
+       helper.playPauseAndUpdate();
        $(this).attr('playState', player.playState);
      });
 
@@ -12,7 +12,7 @@
        if (nextSongIndex >= album.songs.length) { return; }
 
        const nextSong = album.songs[nextSongIndex];
-       player.playPause(nextSong);
+       helper.playPauseAndUpdate(nextSong);
       });
 
       $('#time-control input').on('input', function (event) {
@@ -28,6 +28,25 @@
         $('#time-control input').val(percent);
       }, 1000);
 
+      $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  })
+
+    setInterval( () => {
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+  }, 1000);
+
+    setInterval( () => {
+      if(player.playstate !== 'playing') {return; }
+      const currentVolume = player.setVolume();
+      $('#volume-control input').val();
+    }, 1);
+
       $('button#previous').on('click', function() {
         if (player.playState !== 'playing') { return; }
 
@@ -36,6 +55,6 @@
         if (previousSongIndex < -1) { return; }
 
         const previousSong = album.songs[previousSongIndex];
-        player.playPause(previousSong);
+        helper.playPauseAndUpdate(previousSong);
             });
 }
