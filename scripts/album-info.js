29a61284676d3b6eea50e109a4fe4d4add1aaefc
Cecily Cunningham <soupsuka@gmail.com>
2019-11-08 14:11:35 -0600

    Assignment 15 Album Info: completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtURL);
+  $('.artist').text(album.artist)
+  $('#release-info').text(album.releaseInfo);
 }
