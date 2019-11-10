01ece69e4eb9a25006dfb0e6ca9fb73963e8b3f1
Cecily Cunningham <soupsuka@gmail.com>
2019-11-10 17:55:12 -0600

    Checkpoint 18 Sliders: Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -24,5 +24,18 @@
 
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
-  })
+  });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 }
