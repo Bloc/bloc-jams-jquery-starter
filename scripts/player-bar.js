adc22a6e124451868fcf9ff51716e28b48d7765c
Cecily Cunningham <soupsuka@gmail.com>
2019-11-10 15:10:28 -0600

    Assignment 17 Buttons: Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,8 +10,19 @@
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex <= album.songs.lenth) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  })
 }
