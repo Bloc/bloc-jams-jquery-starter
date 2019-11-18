e23de331ce830604058bb735923729d977d5100d
Cecily Cunningham <soupsuka@gmail.com>
2019-11-18 09:55:12 -0600

    Assignment 18 fixed and completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,8 @@
 class Helper {
    playPauseAndUpdate (song) {
      player.playPause(song);
-     const totalTime = player.getDuration();
-     $('#time-control .total-time').text( player.prettyTime(totalTime) );
+     const totalTime = player.prettyTime(player.currentlyPlaying.duration);
+     $('#time-control .total-time').text( totalTime );
    }
 }
 
