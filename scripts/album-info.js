1d9fd1e112bd4439533d203ee746e9fd0b4cad5d
Mark Elrod <melrod8@gmail.com>
2019-11-21 10:15:51 -0500

    assignment 12-15 make branch

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#release-info').text(album.releaseInfo);
+  $('.artist').text(album.artist);
 }
