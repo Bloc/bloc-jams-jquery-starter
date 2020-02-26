b36331a2bb40d1d2d26c6ec2a51c9037133d2f71
Lauren Rose <lrcolianni@yahoo.com>
2020-02-25 21:14:22 -0600

    v1

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
-  $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-title').text(album.title);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo;
 }
