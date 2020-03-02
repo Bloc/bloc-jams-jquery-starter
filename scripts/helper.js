a4ae262e7c24e69cdfb19591a720625060b44560
Lauren Rose <lrcolianni@yahoo.com>
2020-03-02 10:04:11 -0600

    v10

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,8 +1,12 @@
 
 
 class Helper{
-  
+
   playPauseAndUpdate(song){
     player.playPause(song);
-  }
+
+
+  const duration = player.getDuration();
+  player.getDuration = $('#time-control.total-time').text(duration);
+}
 }
