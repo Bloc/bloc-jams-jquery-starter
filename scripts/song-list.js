aec7887eff263ea55194190930564de45c496384
Jolene Esposito <jolene.art@gmail.com>
2020-03-14 12:35:14 -0400

    add song list, play/pause functionality

---
+++ scripts/song-list.js
@@ -0,0 +1,26 @@
+{
+  album.songs.forEach( (song, index) => {
+    song.element = $(`
+      <tr>
+        <td>
+          <button>
+            <span class="song-number">${index + 1}</span>
+            <span class="ion-play"></span>
+            <span class="ion-pause"></span>
+          </button>
+        </td>  
+        <td>${song.title}</td>
+        <td>${(song.duration /60).toFixed(2)}</td>
+      </tr>
+    `);
+    song.element.on('click', event => {
+      player.playPause(song); //where does 'player' in player.playPause come from?
+    });
+
+    $('#song-list').append(song.element);//why is this placed after the click event?
+  });
+}
+
+$('#song-list').click(function(){
+  playPause(song = this.currentlyPlaying);
+})
