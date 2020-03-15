86f6af2b731779863b66800c2cce2a8754762d63
Jolene Esposito <jolene.art@gmail.com>
2020-03-14 22:00:00 -0400

    add previous track functionality

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,4 +16,15 @@
   /* if (currentSongIndex !== album.songs.length-1){
       player.playPause(nextSong)}; */
   });
+
+  $('button#previous').on('click', function(){
+    if (player.playState !== 'playing'){ return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+
+  });
 }
