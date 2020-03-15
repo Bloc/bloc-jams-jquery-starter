27180c2d0213f2e0138fb07c42721348251e115d
Jolene Esposito <jolene.art@gmail.com>
2020-03-15 12:16:35 -0400

    update time slider

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -23,6 +23,20 @@
     if (previousSongIndex < 0) { return; }
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
+  });
+  $('#time-control input').on('input', function(event){ //why event here and not the other event handlers?
+    player.skipTo(event.target.value); //where did 'event.target' and 'event.target.value' come from?
 
   });
+
+  setInterval( () => { //confused as to why this is written this way
+    if(player.playState !== 'playing') { return; } //why is this line here in this exact position?
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
+
+
 }
