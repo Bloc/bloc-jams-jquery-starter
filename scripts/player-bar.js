f02b101a45cba204e229474bbbdb788e724aaa0a
Katy <katywellington91@gmail.com>
2020-03-26 15:19:28 -0600

    assignment 17 playerbar completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,5 +13,19 @@
 
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
+
   });
+
+  $('button#previous').on('click', function() {
+     if (player.playState !== 'playing') { return; }
+
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const previousSongIndex = currentSongIndex - 1;
+     if (previousSongIndex < 0) { return; }
+
+     const previousSong = album.songs[previousSongIndex];
+     player.playPause(previousSong);
+
+  });
+
 }
