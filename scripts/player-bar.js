6ce9dcbffcc59195943f09f60b442335497abc01
Madi <madison.byteme@gmail.com>
2020-09-18 16:36:16 -0700

    donezo

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -36,6 +36,7 @@
         const duration = player.getDuration();
         const percent = (currentTime / duration) * 100;
    +    $('#time-control .current-time').text( currentTime );
+        $('#time-control .total-time').text (duration);
         $('#time-control input').val(percent);
       }, 1000);
 
